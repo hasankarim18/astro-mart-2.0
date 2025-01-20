0e80c3d94c47fd57f0bd514213531d20eba097c9
hasankarim18 <hallysust@gmail.com>
2025-01-20 22:02:20 +0600

    radio selector

--- assets/astro-main-product-1.js
+++ assets/astro-main-product-1.js
@@ -18,7 +18,9 @@ class AstroVariantSelector extends HTMLElement {
   onVariantChange() {
     this.getSelectedOptions();
     this.getSelectedVariant();
-    console.log(this.options);
+    if (this.currentVariant) {
+      this.updateUrl();
+    }
   }
 
   getSelectedOptions() {
@@ -48,6 +50,16 @@ class AstroVariantSelector extends HTMLElement {
     });
     console.log(this.currentVariant);
   }
+
+  updateUrl() {
+    if (!this.currentVariant) return;
+
+    window.history.replaceState(
+      {},
+      "",
+      `${this.dataset.url}?variant=${this.currentVariant.id}`
+    );
+  }
 }
 
 customElements.define("astro-variant-selector", AstroVariantSelector);
@@ -59,6 +71,14 @@ customElements.define("astro-variant-selector", AstroVariantSelector);
   1. get Selected variants by getSelectedRadios (this is different from <select> </select> ) from the html form
   2. get variantJson embeded from the html  inside first for loop <script type="application/json"> {{ product.variants | json }} </script>
   3. find the specific variantData from the step one array such as ["small", "blue"] and match it with the product.variants from the step 3
+
+  // after finding the variant
+
+  4. we are going to change
+    a. price
+    b. id inside the form hidden tag
+    c. change the url
+    d. change the featured image
 
 */
 
@@ -73,8 +93,13 @@ class AstroVariantRadio extends HTMLElement {
   onRadioSelect() {
     this.getSelectedRadios();
     this.getSelectedVariant();
-
-    console.log("radio values", this.options);
+    if (this.currentVariant) {
+      this.updateUrl();
+      this.updateFeatureImage();
+      this.updateVariantId();
+      this.updateUi();
+    }
+    //   console.log("radio values", this.options);
     // console.log(this.currentVariant);
   }
 
@@ -120,8 +145,94 @@ class AstroVariantRadio extends HTMLElement {
 
       if (findings) return variant;
     });
-    console.log(this.currentVariant);
-    return this.currentVariant;
+    // console.log(this.currentVariant);
+    // return this.currentVariant;
+  }
+
+  updateUrl() {
+    if (!this.currentVariant) return;
+    window.history.replaceState(
+      {},
+      "",
+      `${this.dataset.url}?variant=${this.currentVariant.id}`
+    );
+  }
+
+  updateFeatureImage() {
+    const f_image_tag = document.getElementById("product_feature_image");
+    const currentVariant = this.currentVariant;
+
+    if (currentVariant.featured_image.src !== "") {
+      const variant_img_src = currentVariant.featured_image.src;
+      f_image_tag.src = variant_img_src;
+    } else {
+      console.log("nofeature image");
+    }
+  }
+
+  updateVariantId() {
+    const currentVariant = this.currentVariant;
+    const form_input = document
+      .querySelector("#product_form")
+      .querySelector(`input[name="id"]`);
+    if (!this.currentVariant) return;
+    form_input.value = currentVariant.id;
+  }
+
+  updateUi() {
+    if (!this.currentVariant) return;
+    const buyButtons = document.querySelectorAll("button[data-buy-button]");
+    buyButtons.forEach((btn) => {
+      btn.disabled = true;
+      btn.classList.add("disabled");
+    });
+    fetch(
+      `${this.dataset.url}?variant=${this.currentVariant.id}&section_id=${this.dataset.section}`
+    )
+      .then((response) => {
+        return response.text();
+      })
+      .then((responseText) => {
+        const id = `product_price-${this.dataset.section}`;
+        const html = new DOMParser().parseFromString(responseText, "text/html");
+
+        const oldPrice = document.getElementById(id);
+        const newPrice = html.getElementById(id);
+        if (oldPrice && newPrice) oldPrice.innerHTML = newPrice.innerHTML;
+        // update availability
+        const oldRadio_variant_selector_div = document.getElementById(
+          `radio_variant_selector_div-${this.dataset.section}`
+        );
+        const newRadio_variant_selector_div = html.getElementById(
+          `radio_variant_selector_div-${this.dataset.section}`
+        );
+
+        if (oldRadio_variant_selector_div && newRadio_variant_selector_div)
+          oldRadio_variant_selector_div.innerHTML =
+            newRadio_variant_selector_div.innerHTML;
+
+        // update add tocart section
+        const oldProductAddToCart = document.getElementById(
+          `product_addtocart-${this.dataset.section}`
+        );
+        const newProductAddToCart = html.getElementById(
+          `product_addtocart-${this.dataset.section}`
+        );
+        if (oldProductAddToCart && newProductAddToCart) {
+          oldProductAddToCart.innerHTML = newProductAddToCart.innerHTML;
+        }
+      })
+      .catch((error) => {
+        const errorMessage = document.getElementById("variant_error");
+        errorMessage.classList.remove("thidden");
+        console.error("Error during fetch", error);
+      })
+      .finally(() => {
+        buyButtons.forEach((btn) => {
+          btn.disabled = false;
+          btn.classList.remove("disabled");
+        });
+      });
   }
 }
 
